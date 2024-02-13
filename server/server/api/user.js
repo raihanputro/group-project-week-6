9c38535f5a9efbcb515801387773215cc70ac22c
Raihan162 <muhammadraihanarrafi@gmail.com>
2024-02-13 15:35:44 +0700

    feat: update profile done

--- server/server/api/user.js
+++ server/server/api/user.js
@@ -27,7 +27,7 @@ const changePassword = async (request, reply) => {
 
         const { old_password, new_password, new_confirm_password } = request.body;
 
-        const { id } = request.query
+        const { id } = request.query;
 
         const response = await UserHelper.changePassword(old_password, new_password, new_confirm_password, id);
 
@@ -36,12 +36,30 @@ const changePassword = async (request, reply) => {
             response
         });
     } catch (error) {
-        console.log([fileName, 'Change Password', 'ERROR'], { info: `${error}` });
+        console.log([fileName, 'Change Password API', 'ERROR'], { info: `${error}` });
         return reply.send(GeneralHelper.errorResponse(error));
     }
-}
+};
+
+const updateProfile = async (request, reply) => {
+    try {
+        const { id } = request.query;
+        const { name } = request.body;
+
+        const response = await UserHelper.updateProfile(id, name);
+
+        return reply.send({
+            message: 'Update Profile Success',
+            response
+        });
+    } catch (error) {
+        console.log([fileName, 'Update Profile API', 'ERROR'], { info: `${error}` });
+        return reply.send(GeneralHelper.errorResponse(error));
+    }
+};
 
 Router.get('/my-profile', getProfileUser);
 Router.patch('/change-password', changePassword);
+Router.patch('/update-profile', updateProfile);
 
 module.exports = Router;
--- server/server/helpers/userHelper.js
+++ server/server/helpers/userHelper.js
@@ -71,12 +71,40 @@ const changePassword = async (old_password, new_password, new_confirm_password, 
 
         return Promise.resolve(true);
     } catch (error) {
-        console.log([fileName, 'Change Password', 'ERROR'], { info: `${error}` });
+        console.log([fileName, 'Change Password Helper', 'ERROR'], { info: `${error}` });
+        return Promise.reject(GeneralHelper.errorResponse(error));
+    }
+};
+
+const updateProfile = async (id, name) => {
+    try {
+        const checkUser = await db.User.findOne({
+            where: {
+                id
+            }
+        });
+
+        if (!checkUser) {
+            return Promise.reject(Boom.badRequest('User not found'))
+        };
+
+        await db.User.update({
+            name: name ? name : checkUser.name
+        }, {
+            where: {
+                id
+            }
+        });
+
+        return Promise.resolve(true)
+    } catch (error) {
+        console.log([fileName, 'Update Profile Helper', 'ERROR'], { info: `${error}` });
         return Promise.reject(GeneralHelper.errorResponse(error));
     }
 }
 
 module.exports = {
     getProfileUser,
-    changePassword
-}
+    changePassword,
+    updateProfile
+};
