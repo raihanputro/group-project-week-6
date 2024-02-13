eba0dc743ba0eff1bd287e3726ef2b82a60307d8
Raihan162 <muhammadraihanarrafi@gmail.com>
2024-02-13 16:07:10 +0700

    feat: add my profile

--- client/src/pages/Profile/components/MyProfile/index.jsx
+++ client/src/pages/Profile/components/MyProfile/index.jsx
@@ -1,15 +1,17 @@
 import { FormattedMessage } from 'react-intl';
-import classes from './style.module.scss';
 import { TextField } from '@mui/material';
+
+import classes from './style.module.scss';
 
 const MyProfile = () => {
   return (
-    <div>
+    <div className={classes.container}>
       <h2>
         <FormattedMessage id='profile_myProfile' />
       </h2>
-      <div>
-        <TextField id="outlined-basic" label="Nama" variant="outlined"/>
+      <div className={classes.containerTextField}>
+        <TextField className={classes.name} id="outlined-basic" label={<FormattedMessage id='profile_name' />} variant="outlined" />
+        <TextField className={classes.email} disabled id="outlined-basic" label={<FormattedMessage id='profile_email' />} variant="outlined" />
       </div>
     </div>
   )
