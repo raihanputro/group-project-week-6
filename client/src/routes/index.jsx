68570dfcf6321ec48085bfaa1861394bfb43a942
Raihan162 <muhammadraihanarrafi@gmail.com>
2024-02-13 13:50:34 +0700

    feat: add routes myProfile

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,6 +1,7 @@
 import MainLayout from '@layouts/MainLayout';
 
 import Home from '@pages/Home';
+import Profile from '@pages/Profile';
 import NotFound from '@pages/NotFound';
 
 const routes = [
@@ -9,6 +10,13 @@ const routes = [
     name: 'Home',
     protected: false,
     component: Home,
+    layout: MainLayout,
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    protected: false,
+    component: Profile,
     layout: MainLayout,
   },
   { path: '*', name: 'Not Found', component: NotFound, layout: MainLayout, protected: false },
