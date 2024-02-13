79dcf9ecebc685228d02c4036a66a949fd54db83
Raihan162 <muhammadraihanarrafi@gmail.com>
2024-02-13 15:19:36 +0700

    feat: add change password done

--- server/server/api/user.js
+++ server/server/api/user.js
@@ -2,24 +2,46 @@ const Router = require('express').Router();
 
 const UserHelper = require('../helpers/userHelper');
 const GeneralHelper = require('../helpers/generalHelper');
+const Validation = require('../helpers/validationHelper');
 
 const fileName = 'server/api/user.js';
 
 const getProfileUser = async (request, reply) => {
     try {
-        const {id} = request.query;
+        const { id } = request.query;
 
         const response = await UserHelper.getProfileUser(id);
         return reply.send({
             message: 'Get Profile Success',
             response
-        })
+        });
     } catch (error) {
         console.log([fileName, 'get profile api', 'ERROR'], { info: `${error}` });
+        return reply.send(GeneralHelper.errorResponse(error));
+    }
+};
+
+const changePassword = async (request, reply) => {
+    try {
+        Validation.changePassValidation(request.body);
+
+        const { old_password, new_password, new_confirm_password } = request.body;
+
+        const { id } = request.query
+
+        const response = await UserHelper.changePassword(old_password, new_password, new_confirm_password, id);
+
+        return reply.send({
+            message: 'Change Password Success',
+            response
+        });
+    } catch (error) {
+        console.log([fileName, 'Change Password', 'ERROR'], { info: `${error}` });
         return reply.send(GeneralHelper.errorResponse(error));
     }
 }
 
 Router.get('/my-profile', getProfileUser);
+Router.patch('/change-password', changePassword);
 
 module.exports = Router;
--- server/server/helpers/userHelper.js
+++ server/server/helpers/userHelper.js
@@ -6,23 +6,77 @@ const jwt = require('jsonwebtoken');
 const db = require('../../models');
 const GeneralHelper = require('./generalHelper');
 const fileName = 'server/helpers/userHelper.js';
+const salt = bcrypt.genSaltSync(10);
+
+// eslint-disable-next-line arrow-body-style
+const __hashPassword = (password) => {
+    return bcrypt.hashSync(password, salt);
+};
+
+// eslint-disable-next-line arrow-body-style
+const __comparePassword = (payloadPass, dbPass) => {
+    return bcrypt.compareSync(payloadPass, dbPass);
+};
 
 const getProfileUser = async (id) => {
     try {
         const response = await db.User.findOne({
-            where:{
+            where: {
                 id
             },
             attributes: ['id', 'name', 'email', 'imageUrl', 'role', 'deletedAt']
         });
 
-        return Promise.resolve(response)
+        return Promise.resolve(response);
     } catch (error) {
         console.log([fileName, 'get profile helper', 'ERROR'], { info: `${error}` });
-        return Promise.reject(GeneralHelper.errorResponse(error))
+        return Promise.reject(GeneralHelper.errorResponse(error));
+    }
+};
+
+const changePassword = async (old_password, new_password, new_confirm_password, id) => {
+    try {
+        if (!old_password || !new_password || !new_confirm_password) {
+            return Promise.reject(Boom.badRequest('Must Fill Old Password, New Password and New Confirm Password'))
+        };
+
+        const checkUser = await db.User.findOne({
+            where: {
+                id
+            }
+        });
+
+        const isPassMatch = __comparePassword(old_password, checkUser.password);
+
+        if (!isPassMatch) {
+            return Promise.reject(Boom.badRequest('Wrong Old Password'))
+        };
+
+        if (new_password !== new_confirm_password) {
+            return Promise.reject(Boom.badRequest('New Confirm Password Incorrect'))
+        };
+
+        if (old_password === new_password) {
+            return Promise.reject(Boom.badRequest('New Password Must be Different'))
+        };
+
+        await db.User.update({
+            password: __hashPassword(new_password)
+        },
+            {
+                where: {
+                    id
+                }
+            });
+
+        return Promise.resolve(true);
+    } catch (error) {
+        console.log([fileName, 'Change Password', 'ERROR'], { info: `${error}` });
+        return Promise.reject(GeneralHelper.errorResponse(error));
     }
 }
 
 module.exports = {
-    getProfileUser
+    getProfileUser,
+    changePassword
 }
