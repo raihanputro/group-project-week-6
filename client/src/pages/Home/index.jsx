a5f159339c0cd384114b94b38ae6dad1a5831c53
JambulJambul <hilman_fahmi21@yahoo.com>
2024-02-15 15:57:24 +0700

    create landing page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,10 +1,20 @@
 import { FormattedMessage } from 'react-intl';
+import classes from './style.module.scss'
 
 
 const Home = () => {
   return (
-    <div>
-      <FormattedMessage id="app_greeting" />
+    <div className={classes["page-container"]}>
+      <div className={classes["content-container"]}>
+        <h1>Taskmaster</h1>
+        <h3>We will assist you in building your projects</h3>
+        <button>
+          LOGIN
+        </button>
+        <button>
+          REGISTER
+        </button>
+      </div>
     </div>
   );
 };
