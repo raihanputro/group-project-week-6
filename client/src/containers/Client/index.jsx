c537a375f0af08ddb69ac29b853081052ca4edb7
JambulJambul <hilman_fahmi21@yahoo.com>
2024-02-16 09:33:10 +0700

    restrict page based on role

--- client/src/containers/Client/index.jsx
+++ client/src/containers/Client/index.jsx
@@ -4,9 +4,9 @@ import { useNavigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectLogin } from '@containers/Client/selectors';
+import { selectLogin, selectUserDetails } from '@containers/Client/selectors';
 
-const Client = ({ login, children }) => {
+const Client = ({ role, login, children, userDetails }) => {
   const navigate = useNavigate();
   useEffect(() => {
     if (!login) {
@@ -14,16 +14,42 @@ const Client = ({ login, children }) => {
     }
   }, [login, navigate]);
 
+  if (role == 1) {
+    useEffect(() => {
+      if (userDetails?.role != 1) {
+        navigate('/');
+      }
+    }, [login, navigate]);
+  }
+
+  if (role == 2) {
+    useEffect(() => {
+      if (userDetails?.role != 2) {
+        navigate('/');
+      }
+    }, [login, navigate]);
+  }
+
+  if (role = 3) {
+    useEffect(() => {
+      if (userDetails?.role != 3) {
+        navigate('/');
+      }
+    }, [login, navigate]);
+  }
+
   return children;
 };
 
 Client.propTypes = {
   login: PropTypes.bool,
   children: PropTypes.element,
+  userDetails: PropTypes.object
 };
 
 const mapStateToProps = createStructuredSelector({
   login: selectLogin,
+  userDetails: selectUserDetails
 });
 
 export default connect(mapStateToProps)(Client);
